2830130e45bfc132586e3770bc68db81130a157f
SandroAma <sandroama123@gmail.com>
2022-07-05 23:30:05 -0400

    practice

---
+++ Javascript/More/BigO.js
@@ -0,0 +1,18 @@
+function anotherFunChallenge(input){
+	// let a=5;
+	// let b=10;
+	// for(let i =0; i<input;i++){
+	// 	let x=i+1;
+	// 	let y=i+2;
+	// 	let z=i+3;
+	// 	console.log(x,y,z);
+	// }
+	let whoAmI="I don't know";
+
+	for(let i=0;i<15;i++){
+		console.log(whoAmI);
+	}
+}
+
+anotherFunChallenge(10);
+
