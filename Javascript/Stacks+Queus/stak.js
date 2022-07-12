807f6addfe40b441d864dcdfe599d2c93437d16b
SandroAma <sandroama123@gmail.com>
2022-07-11 23:06:28 -0400

    update stak.js

--- Javascript/Stacks+Queus/stak.js
+++ Javascript/Stacks+Queus/stak.js
@@ -5,25 +5,33 @@
 // Push O(1)
 // Peek O(1)
 
-class Stack{
-    constructor(){
-        this.top = null;
-        this.bottom =null;
-        this.length=0;
+class Node {
+    constructor(value){
+      this.value = value;
+      this.next = null;
     }
-    peek(){
-
+  }
+  
+  class Stack {
+    constructor(){
+      this.top = null;
+      this.bottom = null;
+      this.length = 0;
+    }
+    peek() {
     }
     push(value){
-        
     }
     pop(){
-        
     }
-    isEmpty(){
-        
-    }
-}
-
-
-const myStack = new Stack();
+    //isEmpty
+  }
+  
+  const myStack = new Stack();
+  
+  
+  
+  
+  //Discord
+  //Udemy
+  //google
