c42cd5a18acdd35c4ddc79238c802a685f080538
SandroAma <sandroama123@gmail.com>
2022-07-13 22:52:23 -0400

    Implement insert in bst

--- Javascript/Trees/Binary_Search_Tree.js
+++ Javascript/Trees/Binary_Search_Tree.js
@@ -16,7 +16,32 @@ class Node {
       this.root = null;
     }
     insert(value){
-      //Code here
+      const newNode = new Node(value);
+      if(this.root === null){
+        this.root  = newNode;
+      }
+      else{
+        let currentNode = this.root;
+        while(true){
+            if(value < currentNode.value){
+                // go left 
+                if(!currentNode.left){
+                    currentNode.left = newNode;
+                    return this;
+                } else{
+                    currentNode = currentNode.left;
+                }
+            }else{
+                if(!currentNode.right){
+                    currentNode.right  = newNode;
+                    return this;
+                }
+                else{
+                    currentNode=currentNode.right;
+                }
+            }
+        }
+      }
     }
     lookup(value){
       //Code here
@@ -25,15 +50,18 @@ class Node {
   }
   
   const tree = new BinarySearchTree();
-  tree.insert(9)
-  tree.insert(4)
-  tree.insert(6)
-  tree.insert(20)
-  tree.insert(170)
-  tree.insert(15)
-  tree.insert(1)
-  JSON.stringify(traverse(tree.root))
-  
+  tree.insert(9);
+  tree.insert(4);
+  tree.insert(6);
+  tree.insert(20);
+  tree.insert(170);
+  tree.insert(15);
+  tree.insert(1);
+  JSON.stringify(traverse(tree.root));
+  console.log("");
+  console.log(JSON.stringify(traverse(tree.root)));
+  console.log("");
+
   //     9
   //  4     20
   //1  6  15  170
