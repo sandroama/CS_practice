23ee20858e97fce1578f2ccbed919c15d6f3b6d7
SandroAma <sandroama123@gmail.com>
2022-07-14 23:03:57 -0400

    Practice

---
+++ Javascript/Trees/Trie.js
@@ -0,0 +1,6 @@
+// Trie
+
+// In computer science, a trie, also called digital tree or prefix tree, is a type of k-ary search tree, a tree data structure used for locating specific keys from within a se
+
+// Tries allow to know if a word or a part of the word exists in a text
+
