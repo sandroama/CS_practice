834d872b14987c0fec364d94c92f304557aee011
SandroAma <sandroama123@gmail.com>
2022-07-16 14:21:26 -0400

    Practice Graph

---
+++ Javascript/Graphs/GRPH.js
@@ -0,0 +1,5 @@
+// Types of Graphs
+// Type 1: Directed & Undirected 
+// Type 2: Weighted & Unweighted // Means that it has value for an edge,,, kinda like vector
+// Type 3: Cyclic & Acyclic // Nodes connected to circular fashion then its cyclic
+
