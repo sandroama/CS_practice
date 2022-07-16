4d7115b5b1f95f8124623bdfac8a02a3ff6d9714
SandroAma <sandroama123@gmail.com>
2022-07-16 14:28:24 -0400

    prac

--- Javascript/Trees/Binary_heaps.js
+++ Javascript/Trees/Binary_heaps.js
@@ -4,3 +4,13 @@
 
 // Binary Heap
 // A binary heap is a heap, i.e, a tree which obeys the property that the root of any tree is greater than or equal to (or smaller than or equal to) all its children (heap property). 
+
+
+// Cons: 
+// Slow Lookup
+
+// Pros:
+// Better than O(n) 
+// Priority 
+// Flexible Siize
+// Fast Insert
