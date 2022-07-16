a7938107857ade13398a05d9f072f8f84512f0e6
SandroAma <sandroama123@gmail.com>
2022-07-16 14:24:28 -0400

    Practice

--- Javascript/Graphs/GRPH.js
+++ Javascript/Graphs/GRPH.js
@@ -3,3 +3,8 @@
 // Type 2: Weighted & Unweighted // Means that it has value for an edge,,, kinda like vector
 // Type 3: Cyclic & Acyclic // Nodes connected to circular fashion then its cyclic
 
+// Edge List
+const graph = [[0,2], [2,3],[2,1],[1,3]];
+
+//Adjacent List
+const graph2 = [[2],[2,3],[0,1,3],[1,2]];
