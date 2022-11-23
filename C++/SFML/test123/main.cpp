48718c9c528809a704c26c86fff2ff3953553edb
SandroAma <sandroama123@gmail.com>
2022-11-22 22:16:26 -0500

    added other project files

---
+++ C++/SFML/test123/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Sandro Amaglobeli on 10/25/22.
+//
+
---
+++ C++/Test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int k =100;
+    for (int i = 0; i < k; ++i) {
+        cout<< i<<" "<<i+1<<endl;
+    }
+
+    return 0;
+}
